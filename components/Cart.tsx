c5f8230edb3a007d92d251133b2d3c6867987bc8
hamzaitlalachante <hamzaitlalachante@gmail.com>
2024-01-04 16:31:19 +0100

    modified landpage

---
+++ components/Cart.tsx
@@ -0,0 +1,10 @@
+const Cart = () => {
+    return (
+        <div className="flex flex-col">
+            <h1 className="text-3xl font-bold">Cart</h1>
+            <p className="text-lg">
+                This is the cart page.
+            </p>
+        </div>
+    )
+}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,9 @@ import Link from "next/link"
 import MaxWidthWrapper from "./MaxWidthWrapper"
 import { Icons } from "./Icons"
 import NavItems from "./NavItems"
+import { buttonVariants } from "./ui/button"
+import Cart from './Cart'
+
 
 const Navbar = () => {
     return(
@@ -18,6 +21,18 @@ const Navbar = () => {
                     </div>
                     <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
                         <NavItems />
+                    </div>
+                    <div className="ml-auto flex items-center">
+                    <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space">
+                    {user ? null : (<Link href={'/sign-in'} className={buttonVariants ({variant:'ghost'})}>Sign in </Link>)}
+                    {user ? null : (<span className="h-6 w-px bg-gray-200" aria-hidden="true" />)}
+                    {user ? (<p></p>) : (<Link href={'/sign-up'} className={buttonVariants ({variant:'ghost'})}>Create account </Link>)}
+                    {user ? (<span className="h-6 w-px bg-gray-200" aria-hidden="true" />) : null}
+                    {user ? null : (<div className="felx lg:ml-6"><span className="h-6 w-px bg-gray-200" aria-hidden="true"></span></div>)}
+                    <div className="ml-4 flow-root lg:ml-6">
+                    <Cart/>
+                    </div>
+                    </div>
                     </div>
                 </div>
                 </div>
